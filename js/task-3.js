3927ffcbeb94c6e43d856527ec86348cb5c7ddb9
Serhii-SSD <demydenko.ss@gmail.com>
2026-06-16 19:24:56 +0300

    ^_^

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputer = document.querySelector('input#name-input');
-const output = document.querySelector("span#name-output");
-inputer.addEventListener("input", (event) => {
-    const trimmed = event.currentTarget.value.trim();
-if (trimmed === "") { output.textContent = "Anonymous" }
-    else { output.textContent = trimmed }
-});
